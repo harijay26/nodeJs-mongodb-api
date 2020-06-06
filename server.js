eae2359c8f609e5eca7b5d031c474062f40a23d3
Promise Harry <chinecherempharry@gmail.com>
2020-06-06 11:44:53 +0200

    updated database connection

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 // Database connection
-mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'));
